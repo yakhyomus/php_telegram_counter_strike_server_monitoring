34a1457b2fad746e9d8a0d334e7469b5d385a69d
yakhyomus <80065401+yakhyomus@users.noreply.github.com>
2021-03-07 03:26:30 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+include 'config.php';
+include 'mon.php';
+
+$data = file_get_contents('php://input');
+$data = json_decode($data, true);
+
+if (empty($data['message']['chat']['id'])) {
+	exit();
+}
+
+// Функция вызова методов API.
+function sendTelegram($method, $response)
+{
+	$ch = curl_init('https://api.telegram.org/bot' . TOKEN . '/' . $method);  
+	curl_setopt($ch, CURLOPT_POST, 1);  
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $response);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	$res = curl_exec($ch);
+	curl_close($ch);
+ 
+	return $res;
+}
+
+// Если отправить боту текст или команду
+if (!empty($data['message']['text'])) {
+	$text = $data['message']['text'];
+	if (mb_stripos($text, $command) !== false) {
+		sendTelegram(
+			'sendMessage', 
+			array(
+				'chat_id' => $data['message']['chat']['id'],
+				'text' => $output;
+			)
+		);
+ 
+		exit();	
+	} 
+	exit(ok);	
+	}
+}
+?>
