a9179a923f784c110aa04f398581f76ec7d7b0ef
yakhyomus <80065401+yakhyomus@users.noreply.github.com>
2021-03-07 03:35:47 +0500

    Create mon.php

---
+++ mon.php
@@ -0,0 +1,22 @@
+<?php
+	include 'config.php';
+	include 'query/Buffer.php';
+	include 'query/BaseSocket.php';
+	include 'query/Socket.php';
+	include 'query/SourceQuery.php';
+
+	$sq = new SourceQuery();
+
+
+foreach($servers as $name => $address){
+$sq->Connect($ip, $port, 1, SourceQuery::GOLDSOURCE);
+$aData = $sq->GetInfo();
+$aData['Players'] = $aData['Players'] < 1 ? 0 : $aData['Players'];
+$aData['status'] = strlen($aData['Map']) > 3 ? ''.$aData['Map'].'' : 'не определена';
+$output .= '
+Public #1
+IP: '.$ip.':'.$port.'
+Карта: '.$aData['status'].'
+Игроки: '.$aData['Players'].' из '.$aData['MaxPlayers'].'
+';
+}
